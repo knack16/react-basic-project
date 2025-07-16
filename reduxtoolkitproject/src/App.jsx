79063c7b1b375f1c0122052e868036563e138ebb
Shubham.Tanawade <shubhamtanawade1609@gmail.com>
2025-07-16 21:19:10 +0530

    redux toolkit project

---
+++ reduxtoolkitproject/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todo from './components/Todo'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Redux Toolkit</h1>
+      <AddTodo/>
+      <Todo/>
+    </>
+  )
+}
+
+export default App
